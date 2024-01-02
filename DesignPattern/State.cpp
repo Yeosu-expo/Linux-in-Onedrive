149b20541679a6cf146c5a564b0d33c57aa78945
root <root@DESKTOP-O0V8S6M>
2024-01-02 16:00:43 +0900

    edit state

--- DesignPattern/State.cpp
+++ DesignPattern/State.cpp
@@ -4,64 +4,71 @@ using namespace std;
 
 class State{
 	public:
-		State* getInstance() { return nullptr; };
-		virtual void heal() = 0;
-		virtual void damage() = 0;
+		State* getInstance() { return nullptr; }
+		virtual void heal(State* state) = 0;
+		virtual void damage(State* state) = 0;
 };
 
-class NormalState:public State{
-	static NormalState* normalState;
-	NormalState() {}
+static NormalState* normalState;
+static GoodState* goodState;
+static BadState* badState;
+
+class NormalState:public State{ 
 	public:
-		static State* getInstance(){
-			if(normalState == nullptr)
-				normalState = new NormalState();
-			return normalState;	
-		}
-		void heal(){		
-			cout << "Player is healed" << endl;
-		}
-		void damage(){
-			cout << "Player get damage" << endl;	
-		}
+	static NormalState* normalState;
+	static State* getInstance(){
+		if(normalState == nullptr)
+			normalState = new NormalState();
+		return normalState;	
+	}
+	void heal(State* state){
+		state = GoodState::goodState;	
+		cout << "Player is healed" << endl;
+	}
+	void damage(State* state){
+		state = BadState::badState;
+		cout << "Player get damage" << endl;	
+	}
 };
 
 NormalState* NormalState::normalState =  nullptr;
 
 class GoodState:public State{
-	static GoodState* goodState;
 	GoodState() {}
 	public:
-		static State* getInstance(){
-			if(goodState ==  nullptr)
-				goodState = new GoodState();
-			return goodState;	
-		}
-		void heal(){
-			cout << "Player is too healthy" << endl;	
-		}
-		void damage(){
-			cout << "Player get damage" << endl;	
-		}
+	static GoodState* goodState;
+	static State* getInstance(){
+		if(goodState ==  nullptr)
+			goodState = new GoodState();
+		return goodState;	
+	}
+	void heal(State* state){
+		cout << "Player is too healthy" << endl;	
+	}
+	void damage(State* state){
+		state = NormalState::normalState;
+		cout << "Player get damage" << endl;	
+	}
 };
 
 GoodState* GoodState::goodState = nullptr;
 
 class BadState:public State{
-	static BadState* badState;
 	BadState() {}
 	public:
-		static State* getInstance(){
-			if(badState == nullptr)
-				badState = new BadState();
-			return badState;	
-		}
-		void heal(){
-			cout << "Player is healed" << endl;	
-		}
-		void damage(){
-			cout << "Player can't get any worse" << endl;	
-		}
+	static BadState* badState;
+	static State* getInstance(){
+		if(badState == nullptr)
+			badState = new BadState();
+		return badState;	
+	}
+	void heal(State* state){
+		state = NormalState::normalState;
+		cout << "Player is healed" << endl;	
+	}
+	void damage(State* state){
+		cout << "Player can't get any worse" << endl;	
+	}
 };
 
 BadState* BadState::badState = nullptr;
@@ -70,27 +77,26 @@ class Player{
 	State *state;
 
 	public:
-		Player(){
-			state = NormalState::getInstance();	
-		}
-		void setState(State *state){
-			this->state = state;	
-		}
-		void playerHeal(){
-			state->heal();
-			setState(GoodState::getInstance());	
-		}
-		void playerGetDamege(){
-			state->damage();
-			setState(BadState::getInstance());	
-		}
+	Player(){
+		state = NormalState::getInstance();	
+	}
+	void setState(State *state){
+		this->state = state;	
+	}
+	void playerHeal(){
+		state->heal(state);
+	}
+	void playerGetDamege(){
+		state->damage(state);
+	}
 };
 
 void Client(){
 	Player *player = new Player();
-	
+
 	player->playerHeal();
 	player->playerHeal();
+	player->playerGetDamege();
 	player->playerGetDamege();
 	player->playerGetDamege();
 }
